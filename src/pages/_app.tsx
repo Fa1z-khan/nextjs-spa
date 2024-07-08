1e44c78bf2c744c281de4cddbc577ed2f6430357
Faiz Khan <faiz.khan@everestek.com>
2024-07-08 17:48:33 +0530

    fix: made small changes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,12 @@
-import QueryClientProviderWrapper from "@/Providers/QueryClientProvider";
-import "@/styles/globals.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import type { AppProps } from "next/app";
+import "~/styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const queryClient: QueryClient = new QueryClient();
   return (
-    <QueryClientProviderWrapper>
+    <QueryClientProvider client={queryClient}>
       <Component {...pageProps} />
-    </QueryClientProviderWrapper>
+    </QueryClientProvider>
   );
 }
